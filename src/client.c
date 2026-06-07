36334b8cd8ca089d490a73fd598d2c6e02b0651b
Tombuster <tomekstynjr@gmail.com>
2026-06-07 12:01:40 +0200

    Added basic network functionality

--- src/client.c
+++ src/client.c
@@ -11,7 +11,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <syslog.h>
+
+
+// #define ... ...
 
 
 int
@@ -19,18 +23,33 @@ main(int argc, char** argv){
     // Check args
     if(argc != 2){
         fprintf(stderr, "ERROR: usage: %s <IPv4 address> \n", argv[0]);  // TODO: modify after project is done :)
-                return 1;
+                return -1;
     }
 
     // Initialize socket
-    int local_sockfd;
-    if((local_sockfd = socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP)) < 0){
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    if(( sockfd = socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP)) < 0){
         perror("socket() error");
-        syslog(LOG_ERR, "%s: socket() error", argv[0]);
+        syslog(LOG_ERR, "socket() error : %s\n", strerror(errno)); // TODO: Does the client need syslog?
         return -1;
     }
 
-    
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(2137);
+    if ( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0){
+		syslog(LOG_ERR,"inet_pton() error : %s\n", strerror(errno));
+		return -1;
+	}
+
+    if ( connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0){
+		syslog(LOG_ERR,"connect() error : %s\n", strerror(errno));
+		return 1;
+	}
+
+        // TODO: Handle game logic
 
     exit(0);
 }
--- src/server.c
+++ src/server.c
@@ -10,10 +10,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <unistd.h>
+#include <errno.h>
 #include <syslog.h>
 
 
+#define LISTENQ 5
+
+ 
 int
 main(int argc, char** argv){
     // Check args
@@ -22,16 +27,57 @@ main(int argc, char** argv){
                 return 1;
     }
 
+
+        // TODO: handle SIGCHLD
+
+
     // Initialize listening socket
-    int listenfd;
-    if((listenfd = socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP)) < 0){
+    int listenfd, connfd;
+    struct sockaddr_in servaddr, cliaddr;
+    size_t servlen, clilen;
+    pid_t childpid;
+
+    if(( listenfd = socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP)) < 0){
         perror("socket() error");  // debug info
-        syslog(LOG_ERR, "%s: socket() error", argv[0]);
+        syslog(LOG_ERR, "socket() error : %s\n", strerror(errno));
         return -1;
     }
 
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(2137);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servlen = sizeof(servaddr);
     
+    if ( bind( listenfd, (struct sockaddr *) &servaddr, servlen) < 0){
+            syslog (LOG_ERR,"bind() error : %s\n", strerror(errno));
+            return -1;
+    }
+
+    if ( listen( listenfd, LISTENQ) < 0){
+            syslog (LOG_ERR,"listen() error : %s\n", strerror(errno));
+            return -1;
+    }
     
+    for ( ; ; ) {
+		clilen = sizeof(cliaddr);
+        if ( (connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen)) < 0) {
+			if (errno == EINTR)
+				continue;		/* back to for() */
+			else
+				syslog(LOG_ERR, "accept() error : %s\n", strerror(errno));
+				return -1;
+		}
+
+		if ( (childpid = fork()) == 0) {	/* child process */
+			close(listenfd);	/* close listening socket */
+			/* process the request on connfd*/
+
+                // TODO: process request
+
+			exit(0);
+		}
+    }
 
     exit(0);
 }
